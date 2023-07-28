cea11bd6b8b2f3bf5ecbbf8d8706f51de54cc47f
1dreams1 <81896875+1dreams1@users.noreply.github.com>
2023-07-28 23:40:02 +0800

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ class RunCmd(object):
         subprocess.call(self.cmd, shell=True)
 
 
-# Train the model
+# Train and test the model
 for i in range(3):
         print('train：',i)
         a = RunCmd()
@@ -18,6 +18,12 @@ for i in range(3):
             --posi=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.train.positives.fa \
             --nega=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.train.negatives.fa\
             --model_type=ARSNet --train=True --n_epochs=50')
+    for j in range(10):
+      a = RunCmd()
+      a.cmd_run('python MARSNet.py \
+              --testfile=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.ls.positives.fa \
+              --nega=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.ls.negatives.fa\
+              --model_type=ARSNet --predict=True')
 
 
 
