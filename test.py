395a3cdd4923f1b71e1686537ad65dd2975dfa45
1dreams1 <81896875+1dreams1@users.noreply.github.com>
2023-07-28 23:01:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class RunCmd(object):
 
 # Test the model 
 
-for j in range(1):
+for j in range(10):
       a = RunCmd()
       a.cmd_run('python MARSNet.py \
               --testfile=./GraphProt_CLIP_sequences/ZC3H7B_Baltz2012.ls.positives.fa \
